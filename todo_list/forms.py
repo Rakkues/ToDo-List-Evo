bda351d05171e5a3062cb0b4dfbf332d573d409f
laijian398 <laijian2004@gmail.com>
2026-05-29 01:35:05 +0800

    Update forms.py

--- todo_list/forms.py
+++ todo_list/forms.py
@@ -4,4 +4,4 @@ from .models import List
 class ListForm(forms.ModelForm):
     class Meta:
         model = List
-        fields = ['item', 'completed', 'due_date', 'side_note', 'time_taken', 'priority']
+        fields = ['item', 'completed', 'due_date', 'side_note', 'time_taken', 'priority', 'file', 'image']
