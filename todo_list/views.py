98673250527d46687dd2e0136b18c3ed079f592c
Anas <anaska1311@gmail.com>
2026-06-15 08:55:54 +0800

    Changed add task modal to one already implemented within the page

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,71 +1,67 @@
 from urllib import request
 
-from django.shortcuts import render, redirect
-from django.tasks import task
-from .models import List
-from .forms import ListForm
 from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.tasks import task
+
+from .forms import ListForm
+from .models import List
+
 
 def home(request):
-    if request.method == 'POST':
-        form = ListForm(request.POST, request.FILES) 
-        
+    if request.method == "POST":
+        form = ListForm(request.POST, request.FILES)
+
         if form.is_valid():
             task_instance = form.save(commit=False)
-            task_instance.time_taken = form.cleaned_data.get('time_taken')
+            task_instance.time_taken = form.cleaned_data.get("time_taken")
             task_instance.save()
             messages.success(request, ("Task Has Been Added To List!"))
-            return redirect('home')  
+            return redirect("home")
 
     else:
-        form = ListForm() 
+        form = ListForm()
 
     tasks = List.objects.all()
-    return render(request, 'home.html', {'tasks': tasks, 'form': form})
-
-
-def about(request):
-	context = {'first_name': 'Alireza', 'last_name': 'Ghorbani'}
-	return render(request, 'about.html', context)
+    return render(request, "home.html", {"tasks": tasks, "form": form})
 
 
 def delete(request, list_id):
-	task = List.objects.get(pk=list_id)
-	task.delete()
-	messages.success(request, ("Task Has Been Deleted!"))
-	return redirect('home')
+    task = List.objects.get(pk=list_id)
+    task.delete()
+    messages.success(request, ("Task Has Been Deleted!"))
+    return redirect("home")
 
 
 def cross_off(request, list_id):
-	task = List.objects.get(pk=list_id)
-	task.completed = True
-	task.save()
-	return redirect('home')
+    task = List.objects.get(pk=list_id)
+    task.completed = True
+    task.save()
+    return redirect("home")
+
 
 def uncross(request, list_id):
-	task = List.objects.get(pk=list_id)
-	task.completed = False
-	task.save()
-	return redirect('home')
+    task = List.objects.get(pk=list_id)
+    task.completed = False
+    task.save()
+    return redirect("home")
 
 
 def edit(request, list_id):
     task = List.objects.get(pk=list_id)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = ListForm(request.POST or None, request.FILES or None, instance=task)
         if form.is_valid():
             task_instance = form.save(commit=False)
-            task_instance.time_taken = form.cleaned_data.get('time_taken')
+            task_instance.time_taken = form.cleaned_data.get("time_taken")
             task_instance.save()
-            messages.success(request, ('Task Has Been Edited!'))
-            return redirect('home')
+            messages.success(request, ("Task Has Been Edited!"))
+            return redirect("home")
     else:
         form = ListForm(instance=task)
-        
-    return render(request, 'edit.html', {'task': task, 'form': form})
-	
-	
+
+    return render(request, "edit.html", {"task": task, "form": form})
 
 
 # password12345
